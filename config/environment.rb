e4b3de0c8f3e4d699ae6543788d8e02df1d608ea
Johnathan Martin <john.martin@stanford.edu>
2016-02-05 03:28:30 -0800

    initial configuration implementation
    * create the config structure dor-services expects
    * populate it with values from config gem via 'Settings'
    * add default settings.yml
    * load the dor-services config

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# load the dor-services config
+require File.join(Rails.root, 'config', 'dor_config')
+
 # Initialize the Rails application.
 Rails.application.initialize!
