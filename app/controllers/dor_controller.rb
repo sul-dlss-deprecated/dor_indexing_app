4c2696247f307561601a39e88f077e99bbd4fc0f
Johnathan Martin <john.martin@stanford.edu>
2016-02-05 03:33:21 -0800

    initial reindex method implementation

--- app/controllers/dor_controller.rb
+++ app/controllers/dor_controller.rb
@@ -1,6 +1,9 @@
 class DorController < ApplicationController
-  ##
-  # TODO: Implement reindexing route here
   def reindex
+    @solr_doc = Dor::IndexingService.reindex_pid params[:pid], Dor::IndexingService.generate_index_logger { request.uuid }
+    Dor::SearchService.solr.commit # reindex_pid doesn't commit, but callers of this method may expect the update to be committed immediately
+    render status: 200, text: "Successfully updated index for #{params[:pid]}"
+  rescue ActiveFedora::ObjectNotFoundError # => e
+    render status: 404, text: 'Object does not exist in Fedora.'
   end
 end
