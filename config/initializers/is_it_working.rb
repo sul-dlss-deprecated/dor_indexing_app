749e70ecbfb02f517f3fdd20a9caa988de892f9e
Johnathan Martin <john.martin@stanford.edu>
2016-02-19 17:37:32 -0800

    switch to is_it_working-cbeer and add fedora and solr checks
    note: only configure the check for environments other than test, so that fedora and solr are actually configured, so rspec doesn't crash because it can't find cert & key files.

--- config/initializers/is_it_working.rb
+++ config/initializers/is_it_working.rb
@@ -1,4 +1,8 @@
 require 'is_it_working'
 
-Rails.configuration.middleware.use(IsItWorking::Handler) do |h|
+unless Rails.env == 'test'
+  Rails.configuration.middleware.use(IsItWorking::Handler) do |h|
+    h.check :rubydora, :client => ActiveFedora::Base.connection_for_pid(0)
+    h.check :rsolr, :client => Dor::SearchService.solr
+  end
 end
