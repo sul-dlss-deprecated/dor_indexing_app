4752021186feccd72cef95b50e0c5d7f38033edf
Chris Beer <chris@cbeer.info>
2016-10-20 07:22:56 -0700

    Update to Rails 5

--- app/controllers/dor_controller.rb
+++ app/controllers/dor_controller.rb
@@ -2,8 +2,8 @@ class DorController < ApplicationController
   def reindex
     @solr_doc = Dor::IndexingService.reindex_pid params[:pid], logger: Dor::IndexingService.generate_index_logger { request.uuid }, add_attributes: { commitWithin: params.fetch(:commitWithin, 1000).to_i }
     Dor::SearchService.solr.commit unless params[:commitWithin] # reindex_pid doesn't commit, but callers of this method may expect the update to be committed immediately
-    render status: 200, text: "Successfully updated index for #{params[:pid]}"
+    render status: 200, plain: "Successfully updated index for #{params[:pid]}"
   rescue ActiveFedora::ObjectNotFoundError # => e
-    render status: 404, text: 'Object does not exist in Fedora.'
+    render status: 404, plain: 'Object does not exist in Fedora.'
   end
 end
