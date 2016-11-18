cea5c3d56176db595b4ff1d1f9659591c5435d55
Darren Hardy <drh@stanford.edu>
2016-11-18 10:59:30 -0800

    add dev environment and cleanup

--- config/deploy/legacy.rb
+++
@@ -1,6 +0,0 @@
-server "dor-indexing-app-legacy-prod.stanford.edu", user: fetch(:user), roles: %w{web app}
-
-set :rails_env, 'production'
-set :bundle_without, %w(test development).join(' ')
-
-Capistrano::OneTimeKey.generate_one_time_key!
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,6 +3,6 @@ server 'dor-indexing-app-prod-b.stanford.edu', user: fetch(:user), roles: %w{web
 server 'dor-indexing-app-prod-c.stanford.edu', user: fetch(:user), roles: %w{web app}
 
 set :rails_env, 'production'
-set :bundle_without, %w(test development).join(' ')
+set :bundle_without, %w(test development deployment).join(' ')
 
 Capistrano::OneTimeKey.generate_one_time_key!
