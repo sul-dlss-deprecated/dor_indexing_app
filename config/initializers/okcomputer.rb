8a6ff72fe938ae6561a1e9745a772c668e805b50
Justin Coyne <jcoyne@justincoyne.com>
2019-09-03 07:28:55 -0500

    Avoid deprecated Dor::SearchService

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -20,7 +20,7 @@ OkComputer::Registry.register 'version', VersionCheck.new
 ##
 # EXTERNAL Services
 
-OkComputer::Registry.register 'external-solr', OkComputer::HttpCheck.new(Dor::SearchService.solr.uri.to_s.gsub(/\/$/, '') + '/admin/ping')
+OkComputer::Registry.register 'external-solr', OkComputer::HttpCheck.new(ActiveFedora.solr.conn.uri.to_s.gsub(/\/$/, '') + '/admin/ping')
 
 # Simple check to ping the Fedora server by asking it to describe the repository
 class FedoraCheck < OkComputer::Check
