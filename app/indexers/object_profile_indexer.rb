cc6e57648a16ba1ab0840336b4fb51e7fdcef36a
Justin Coyne <jcoyne@justincoyne.com>
2020-03-12 17:13:25 -0500

    Add an object profile indexer

---
+++ app/indexers/object_profile_indexer.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class ObjectProfileIndexer
+  include SolrDocHelper
+
+  attr_reader :resource
+
+  def initialize(resource:)
+    @resource = resource
+  end
+
+  # @return [Hash] the partial solr document for releasable concerns
+  def to_solr
+    return {} unless inner_object.respond_to?(:profile) # Skip unsaved items
+
+    {}.tap do |solr_doc|
+      inner_object.profile.each_pair do |property, value|
+        add_solr_value(solr_doc, property.underscore, value, (property =~ /Date/ ? :date : :symbol), [:stored_searchable])
+      end
+    end
+  end
+
+  # rubocop:disable Lint/UselessAccessModifier
+  private
+
+  # rubocop:enable Lint/UselessAccessModifier
+
+  delegate :inner_object, to: :resource
+end
