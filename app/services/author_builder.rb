56cbb9bc589cb98bd40c3f9c6177e6b6f203dba6
Naomi Dushay <ndushay@stanford.edu>
2021-03-25 15:56:13 -0700

    appease rubocop masters

--- app/services/author_builder.rb
+++ app/services/author_builder.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 class AuthorBuilder
-  ALLOWED_ROLES = %w[ Author creator ].freeze
+  ALLOWED_ROLES = %w[Author creator].freeze
 
   # @param [Array<Cocina::Models::Contributor>] contributors
   # @returns [String] The partial solr document
   def self.build(contributors)
     contributors
-      .reject { |contributor| contributor.role && !ALLOWED_ROLES.include?(contributor.role.first.value) }
+      .reject { |contributor| contributor.role && ALLOWED_ROLES.exclude?(contributor.role.first.value) }
       .flat_map do |contributor|
         contributor.name.map do |name|
           next name.value if name.value
