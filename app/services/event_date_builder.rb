b96199fc515b442fa4e30ed42a7f15e5090f0f6f
Naomi Dushay <ndushay@stanford.edu>
2021-04-05 15:00:50 -0700

    event_date-builder: allow untyped date value per algorithm; refactoring

--- app/services/event_date_builder.rb
+++ app/services/event_date_builder.rb
@@ -7,18 +7,18 @@ class EventDateBuilder
     event_dates = Array(event&.date) + Array(event&.parallelEvent&.map(&:date))
 
     matching_date_value_with_status_primary(event_dates, date_type) ||
-      matching_date_value(event_dates, date_type)
+      matching_date_value(event_dates, date_type) ||
+      untyped_date_value(event_dates)
   end
 
   # @return [String, nil] date.value from a date of type of date_type and of status primary
   def self.matching_date_value_with_status_primary(event_dates, date_type)
     event_dates.flatten.compact.find do |date|
       next if date.type != date_type
-      return date.value if date.status == 'primary' && date&.value.present?
 
-      Array(date&.structuredValue).find do |structured_date|
-        return structured_date.value if structured_date.status == 'primary'
-      end
+      next unless EventSelector.date_status_primary(date)
+
+      return date_value(date)
     end
   end
   private_class_method :matching_date_value_with_status_primary
@@ -27,12 +27,36 @@ class EventDateBuilder
   def self.matching_date_value(event_dates, date_type)
     event_dates.flatten.compact.find do |date|
       next if date.type != date_type
-      return date.value if date&.value.present?
 
-      Array(date&.structuredValue).find do |structured_date|
-        return structured_date.value if structured_date&.value.present?
-      end
+      return date_value(date)
     end
   end
   private_class_method :matching_date_value
+
+  # @return [String, nil] date.value from a date without a type
+  def self.untyped_date_value(event_dates)
+    event_dates.flatten.compact.find do |date|
+      next if date.type.present?
+
+      return date_value(date)
+    end
+  end
+  private_class_method :untyped_date_value
+
+  # @param [Cocina::Models::DescriptiveValue] a date object from an event
+  # @return [String, nil] value from date object
+  # rubocop:disable Metrics/PerceivedComplexity
+  def self.date_value(date)
+    return date.value if date&.value.present?
+
+    Array(date&.structuredValue).find do |structured_value|
+      return structured_value.value if structured_value&.value.present?
+    end
+
+    Array(date&.parallelValue).find do |parallel_value|
+      return parallel_value.value if parallel_value&.value.present?
+    end
+  end
+  # rubocop:enable Metrics/PerceivedComplexity
+  private_class_method :date_value
 end
