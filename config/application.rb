c63a6f90adff3c43eb75f25a7a7009661b0cb15f
Justin Coyne <jcoyne@justincoyne.com>
2021-12-16 16:44:21 -0600

    Upgrade to rails 7

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require_relative 'boot'
 require 'rails'
 # Pick the frameworks you want:
 require 'active_model/railtie'
-require 'active_job/railtie'
+# require "active_job/railtie"
 # require "active_record/railtie"
 # require "active_storage/engine"
 require 'action_controller/railtie'
@@ -14,8 +14,7 @@ require 'action_controller/railtie'
 # require "action_text/engine"
 require 'action_view/railtie'
 # require "action_cable/engine"
-# require "sprockets/railtie"
-require 'rails/test_unit/railtie'
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -40,17 +39,10 @@ class JSONAPIError < Committee::ValidationError
   end
 end
 
-# Module surrounding Rails application
 module DorIndexingApp
-  # Entrypoint to Rails application
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.0
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
 
     # Do not validate e.g. OKComputer routes using OpenAPI
     accept_proc = proc { |request| request.path.start_with?('/dor') }
@@ -64,6 +56,14 @@ module DorIndexingApp
     # TODO: Uncomment when API returns JSON or when Committee allows validating plain-text responses
     #
     # config.middleware.use Committee::Middleware::ResponseValidation, schema_path: 'openapi.yml'
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
