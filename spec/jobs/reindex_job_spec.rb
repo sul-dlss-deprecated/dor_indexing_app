8e2a49907967fbeea244189e71ef89f34c7f83ac
Justin Coyne <jcoyne@justincoyne.com>
2022-02-07 08:35:08 -0600

    Index documents without having to do an API call

--- spec/jobs/reindex_job_spec.rb
+++ spec/jobs/reindex_job_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe ReindexJob do
-  let(:message) { { model: model }.to_json }
+  let(:message) { { model: model, created_at: 'Wed, 01 Jan 2021 12:58:00 GMT', modified_at: 'Wed, 03 Mar 2021 18:58:00 GMT' }.to_json }
   let(:druid) { 'druid:bc123df4567' }
 
   let(:model) do
@@ -16,7 +16,7 @@ RSpec.describe ReindexJob do
   end
 
   let(:result) { Success(double) }
-  let(:indexer) { instance_double(Indexer, reindex_pid: true, fetch_model_with_metadata: result) }
+  let(:indexer) { instance_double(Indexer, reindex_pid: true) }
 
   before do
     allow(Indexer).to receive(:new).and_return(indexer)
@@ -25,6 +25,6 @@ RSpec.describe ReindexJob do
   it 'updates the druid' do
     described_class.new.work(message)
     expect(indexer).to have_received(:reindex_pid)
-      .with(add_attributes: { commitWithin: 1000 }, cocina_with_metadata: result)
+      .with(add_attributes: { commitWithin: 1000 }, cocina_with_metadata: Success(Array))
   end
 end
