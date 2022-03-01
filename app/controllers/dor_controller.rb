55eaf78e910ac809e777292c629e1e5bd50792f2
Johnathan Martin <john.martin@stanford.edu>
2022-03-01 00:57:00 -0800

    DorController#reindex: allow caller to provide cocina

--- app/controllers/dor_controller.rb
+++ app/controllers/dor_controller.rb
@@ -2,17 +2,21 @@
 
 # Main controller of application
 class DorController < ApplicationController
+  include Dry::Monads[:result]
+
+  class CocinaModelBuildError < StandardError; end
+
   def reindex
-    indexer = Indexer.new(solr: solr, pid: params[:pid])
-    cocina_with_metadata = indexer.fetch_model_with_metadata
-    @solr_doc = indexer.reindex_pid(
-      add_attributes: { commitWithin: params.fetch(:commitWithin, 1000).to_i },
-      cocina_with_metadata: cocina_with_metadata
-    )
-    indexer.commit unless params[:commitWithin] # reindex_pid doesn't commit, but callers of this method may expect the update to be committed immediately
+    @indexer = Indexer.new(solr: solr, pid: params[:pid])
+
+    reindex_pid
     render status: :ok, plain: "Successfully updated index for #{params[:pid]}"
   rescue Dor::Services::Client::NotFoundResponse, Rubydora::RecordNotFound
     render status: :not_found, plain: 'Object does not exist in the repository'
+  rescue CocinaModelBuildError => e
+    request.session # TODO: calling this as a hack to address bad Rails/HB interaction, remove when https://github.com/rails/rails/issues/43922 is fixed
+    Honeybadger.notify('Error building Cocina model', context: { druid: params[:pid], build_error: e.cause.message }, backtrace: e.cause.backtrace)
+    render status: :unprocessable_entity, plain: "Error building Cocina model for #{params[:pid]}"
   end
 
   def delete_from_index
@@ -23,7 +27,35 @@ class DorController < ApplicationController
 
   private
 
+  attr_reader :indexer
+
   def solr
     RSolr.connect(timeout: 120, open_timeout: 120, url: Settings.solrizer_url)
+  end
+
+  def build_model_and_metadata(cocina_model_json:, created_at:, updated_at:)
+    model = Cocina::Models.build(JSON.parse(cocina_model_json))
+    metadata = Dor::Services::Client::ObjectMetadata.new(created_at: created_at, updated_at: updated_at)
+    Success([model, metadata])
+  rescue StandardError
+    raise CocinaModelBuildError
+  end
+
+  # @returns [Success,Failure] the result of finding/parsing the model with metadata
+  def cocina_with_metadata
+    cocina_model_json = params[:cocina_model_json].presence
+    created_at = params[:created_at].presence
+    updated_at = params[:updated_at].presence
+    return indexer.fetch_model_with_metadata unless cocina_model_json && created_at && updated_at
+
+    build_model_and_metadata(cocina_model_json: cocina_model_json, created_at: created_at, updated_at: updated_at)
+  end
+
+  def reindex_pid
+    @solr_doc = indexer.reindex_pid(
+      add_attributes: { commitWithin: params.fetch(:commitWithin, 1000).to_i },
+      cocina_with_metadata: cocina_with_metadata
+    )
+    indexer.commit unless params[:commitWithin] # reindex_pid doesn't commit, but callers of this method may expect the update to be committed immediately
   end
 end
--- spec/requests/dor_requests_spec.rb
+++ spec/requests/dor_requests_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe 'DOR', type: :request do
     let(:connection) { instance_double(Rubydora::Repository) }
     let(:mock_logger) { instance_double(Logger, :formatter= => true, info: true) }
     let(:mock_solr_conn) { instance_double(RSolr::Client, add: true, commit: true) }
-    let(:metadata) { {} }
+    let(:metadata) { instance_double(Dor::Services::Client::ObjectMetadata) }
     let(:cocina) { instance_double(Cocina::Models::DRO) }
     let(:object_service) { instance_double(Dor::Services::Client::Object, find_with_metadata: [cocina, metadata]) }
     let(:mock_indexer) { instance_double(CompositeIndexer::Instance, to_solr: mock_solr_doc) }
@@ -53,6 +53,46 @@ RSpec.describe 'DOR', type: :request do
       post "/dor/reindex/#{druid}"
       expect(response.body).to eq 'Object does not exist in the repository'
       expect(response.code).to eq '404'
+    end
+
+    context 'when cocina is provided by caller' do
+      let(:cocina_model_json) { '{ "some": "json" }' }
+      let(:cocina_model_metadata) { instance_double(Dor::Services::Client::ObjectMetadata) }
+      let(:created_at) { '2022-02-27' }
+      let(:updated_at) { '2022-02-28' }
+
+      before do
+        allow(Dor::Services::Client::ObjectMetadata).to receive(:new).with(created_at: created_at, updated_at: updated_at).and_return(metadata)
+      end
+
+      it 'uses the provided cocina without hitting dor-services-app' do
+        allow(Cocina::Models).to receive(:build).with(JSON.parse(cocina_model_json)).and_return(cocina) # pretend our bogus test JSON is valid
+        post "/dor/reindex/#{druid}", params: { cocina_model_json: cocina_model_json, created_at: created_at, updated_at: updated_at }
+        expect(response.body).to eq "Successfully updated index for #{druid}"
+        expect(response.code).to eq '200'
+        expect(mock_solr_conn).to have_received(:add).with(mock_solr_doc, add_attributes: { commitWithin: 1000 })
+        expect(object_service).not_to have_received(:find_with_metadata)
+      end
+
+      it 'requires both the cocina json and the created_at/updated_at metadata' do
+        post "/dor/reindex/#{druid}", params: { cocina_model_json: cocina_model_json, updated_at: updated_at }
+        expect(response.code).to eq '200'
+        expect(mock_solr_conn).to have_received(:add).with(mock_solr_doc, add_attributes: { commitWithin: 1000 })
+        expect(object_service).to have_received(:find_with_metadata)
+      end
+
+      it 'provides the caller with a useful error if invalid cocina is provided' do
+        # Cocina::Models.build will be called with our bogus JSON, which will throw an error
+        allow(Honeybadger).to receive(:notify).and_call_original
+        post "/dor/reindex/#{druid}", params: { cocina_model_json: cocina_model_json, created_at: created_at, updated_at: updated_at }
+        expect(response.code).to eq '422' # the caller should've provided valid Cocina JSON
+        expect(response.body).to eq "Error building Cocina model for #{druid}"
+        expect(Honeybadger).to have_received(:notify) do |msg, context:, backtrace:|
+          expect(msg).to eq 'Error building Cocina model'
+          expect(context).to eq({ druid: druid, build_error: 'key not found: "type"' }) # TODO: this will change with https://github.com/sul-dlss/cocina-models/issues/318
+          expect(backtrace).to include(/cocina-models/)
+        end
+      end
     end
   end
 
