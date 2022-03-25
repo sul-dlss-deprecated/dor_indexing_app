358336d633ed236f9913cb21b26d467ecdcef6b2
Justin Coyne <jcoyne@justincoyne.com>
2022-03-25 09:10:12 -0500

    Avoid the fallback indexer when doing rolling indexing

--- spec/jobs/reindex_by_druid_job_spec.rb
+++ spec/jobs/reindex_by_druid_job_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe ReindexByDruidJob do
   let(:message) { { druid: druid }.to_json }
   let(:druid) { 'druid:bc123df4567' }
 
-  let(:indexer) { instance_double(Indexer, reindex: true) }
+  let(:indexer) { instance_double(Indexer, load_and_index: true) }
 
   before do
     allow(Indexer).to receive(:new).and_return(indexer)
@@ -21,20 +21,18 @@ RSpec.describe ReindexByDruidJob do
 
     it 'updates the druid' do
       described_class.new.work(message)
-      expect(indexer).to have_received(:reindex)
-        .with(add_attributes: { commitWithin: 1000 }, cocina_with_metadata: result)
+      expect(indexer).to have_received(:load_and_index)
     end
   end
 
   context 'when object is not found' do
     before do
-      allow(indexer).to receive(:fetch_model_with_metadata).and_raise(Dor::Services::Client::NotFoundResponse)
+      allow(indexer).to receive(:load_and_index).and_raise(Dor::Services::Client::NotFoundResponse)
       allow(Honeybadger).to receive(:notify)
     end
 
     it 'does not update the druid' do
       described_class.new.work(message)
-      expect(indexer).not_to have_received(:reindex)
       expect(Honeybadger).to have_received(:notify)
     end
   end
