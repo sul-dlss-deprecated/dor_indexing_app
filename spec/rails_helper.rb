74aba4f9682e35792533d3118ea32512ff90cd48
Justin Littman <justinlittman@stanford.edu>
2022-05-27 08:48:07 -0400

    Remove unused dry config from rails_helper.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,9 +40,6 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
-  config.include(Dry::Monads[:result])
-  config.include(Dry::Monads[:maybe])
-
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
