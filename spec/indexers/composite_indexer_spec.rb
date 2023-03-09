823e5bf1f69b3b3b2a860db2987949f6caefde8e
Michael J. Giarlo <mjgiarlo@stanford.edu>
2023-03-09 10:24:37 -0800

    Remove indexing of single-valued metadata source field
    Fixes #947
    
    This commit removes metadata source indexing that is no longer needed after the Folio migration is complete.

--- spec/indexers/composite_indexer_spec.rb
+++ spec/indexers/composite_indexer_spec.rb
@@ -47,7 +47,6 @@ RSpec.describe CompositeIndexer do
         'sw_display_title_tesim' => 'Test item',
         'nonhydrus_apo_title_ssim' => ['test admin policy'],
         'apo_title_ssim' => ['test admin policy'],
-        'metadata_source_ssi' => 'DOR',
         'metadata_source_ssim' => ['DOR'],
         'objectId_tesim' => ['druid:mx123ms3333', 'mx123ms3333'],
         'topic_ssim' => ['word'],
