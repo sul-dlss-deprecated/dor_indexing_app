16731b0d8fb33369f051d6ca65deecbf1d1cb842
Aaron Collier <aaron.collier@stanford.edu>
2023-09-25 09:52:42 -0700

    Update sw_subject_temporal_ssim to use Stanford::Mods::SearchworksSubjects.era_facet

--- app/indexers/descriptive_metadata_indexer.rb
+++ app/indexers/descriptive_metadata_indexer.rb
@@ -23,7 +23,7 @@ class DescriptiveMetadataIndexer
       'sw_author_tesim' => author,
       'contributor_orcids_ssim' => orcids,
       'sw_display_title_tesim' => title,
-      'sw_subject_temporal_ssim' => subject_temporal,
+      'sw_subject_temporal_ssim' => stanford_mods_record.era_facet,
       'sw_subject_geographic_ssim' => subject_geographic,
       'sw_pub_date_facet_ssi' => pub_year,
       'originInfo_date_created_tesim' => creation_date,
